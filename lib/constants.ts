50398064879499a0c3fa647f4f90bf157b536625
AxlGuillen <axl1504@hotmail.com>
2026-04-19 23:10:58 -0600

    Add shared constants file

---
+++ lib/constants.ts
@@ -0,0 +1,11 @@
+export const WA_NUMBER = "524435145662";
+export const WA_URL = `https://wa.me/${WA_NUMBER}?text=${encodeURIComponent("Hola, me interesa cotizar materias primas")}`;
+
+export const FACEBOOK_URL = "https://www.facebook.com/profile.php?id=100094681106351";
+
+export const CONTACT_EMAIL = "ventas@aditmex.com.mx";
+
+export const ADDRESS = "Santos Degollado 595, Col. Nueva Chapultepec, 58280 Morelia, Mich.";
+
+export const MAP_EMBED_URL =
+  "https://maps.google.com/maps?q=Santos+Degollado+595,+Nueva+Chapultepec,+58280+Morelia,+Michoacan,+Mexico&t=&z=16&ie=UTF8&iwloc=&output=embed";
